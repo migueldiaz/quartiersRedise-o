d2493efd85f1d333dfe2d705878316868609f051
juanantonioruz <juanantonioruz@gmail.com>
2011-08-29 00:06:45 +0200

    mejorando interfaz zona de gestion

--- app/helpers/sitios_helper.rb
+++ app/helpers/sitios_helper.rb
@@ -1,2 +1,3 @@
 module SitiosHelper
+
 end
--- app/helpers/web_helper.rb
+++ app/helpers/web_helper.rb
@@ -1,2 +1,18 @@
 module WebHelper
+
+  
+    def comprueba_admin_sel sitio_name, sitio
+    if !sitio.nil?
+      if sitio_name == 'jeunes' and !sitio.jeunes.nil? then 
+        return "seleccionado"
+        else if  sitio_name == 'asociacion' and !sitio.asociacion.nil? then
+        return "seleccionado"
+        else if  sitio_name == 'femmes' and !sitio.femmes.nil? then
+        return "seleccionado"
+        end
+        end
+     end
+  end
+  
+  end 
 end
