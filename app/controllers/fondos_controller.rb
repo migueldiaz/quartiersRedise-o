539d0d032e5ff5e53f37d8a301166004cc46aa3f
juanantonioruz <juanantonioruz@gmail.com>
2011-09-14 11:58:34 +0200

    Closes #81 el copy/paste es una fuente de errores

--- app/controllers/fondos_controller.rb
+++ app/controllers/fondos_controller.rb
@@ -4,54 +4,54 @@ class FondosController < ApplicationController
   layout 'mono'
   
   def index
-  @sitio=Sitio.find(params[:id])
-  @fondos=@sitio.fondos
+    @sitio=Sitio.find(params[:id])
+    @fondos=@sitio.fondos
   end
   def new
-  @sitio=Sitio.find(params[:id])
-  @fondo=@sitio.fondos.new
-  
+    @sitio=Sitio.find(params[:id])
+    @fondo=@sitio.fondos.new
+    
   end
   
   def create
-   
+    
     @fondo = Fondo.create(params[:fondo])
     
     #@pagina=@documento.pagina
     
-     respond_to do |format|
-     	 if @fondo.save
-       	 format.html { redirect_to(@fondo, :notice =>  t('exito')) }
-        	format.xml  { render :xml => @fondo, :status => :created, :location => @fondo }
-     	 else
-      	  format.html { render :action => "new" }
-       	 format.xml  { render :xml => @fondo.errors, :status => :unprocessable_entity }
-     	 end
+    respond_to do |format|
+      if @fondo.save
+        format.html { redirect_to(@fondo, :notice =>  t('exito')) }
+        format.xml  { render :xml => @fondo, :status => :created, :location => @fondo }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @fondo.errors, :status => :unprocessable_entity }
+      end
     end
     
-end
-def show
-   @fondo = Fondo.find(params[:id])
-   @sitio = @fondo.sitio
-   
-end
- def destroy
+  end
+  def show
+    @fondo = Fondo.find(params[:id])
+    @sitio = @fondo.sitio
+    
+  end
+  def destroy
     @fondo = Fondo.find(params[:id])
     @sitio=@fondo.sitio
     @fondo.destroy
     redirect_to fondos_path(:id=>@sitio)
-   
-end
-def edit
-   @fondo = Fondo.find(params[:id])
-end 
-
-def update
-   @fondo = fondo.find(params[:id])
- #  @pagina = Pagina.find(@documento.pagina)
-
+    
+  end
+  def edit
+    @fondo = Fondo.find(params[:id])
+  end 
+  
+  def update
+    @fondo = Fondo.find(params[:id])
+    #  @pagina = Pagina.find(@documento.pagina)
+    
     respond_to do |format|
-      if @fondo.update_attributes(params[:imagen])
+      if @fondo.update_attributes(params[:fondo])
         format.html { redirect_to(@fondo, :notice => t('exitom')) }
         format.xml  { head :ok }
       else
@@ -59,7 +59,7 @@ def update
         format.xml  { render :xml => @fondo.errors, :status => :unprocessable_entity }
       end
     end
-end
+  end
   
   
 end
