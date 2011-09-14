8631794e715e6d523d59c9961a0c8231e032f9e0
juanantonioruz <juanantonioruz@gmail.com>
2011-09-14 10:48:30 +0200

    conversion de fecha en foro traspaso de informacion

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -9,4 +9,15 @@ class Comentario < ActiveRecord::Base
 	def valida_comentario
   		errors.add_to_base 'avisocomentario' if textoes.blank? and textofr.blank? ||  tituloes.blank? and titulofr.blank?
 	end
+  
+   def created_at=(date)
+    fragmentos=date.to_s.split
+    fecha=fragmentos[0].split("/")
+    fecha_format=[fecha[2], fecha[1],fecha[0]].join("/")
+    logger.info("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"+date)
+    logger.info("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"+fecha_format+" "+fragmentos[1])
+    
+        write_attribute(:created_at,fecha_format+" "+fragmentos[1])
+
+  end
 end
--- config/initializers/date_formats.rb
+++
@@ -1,5 +0,0 @@
-Time::DATE_FORMATS.merge!(
-:standard => '%d %b %Y',
-:default => '%d/%m/%Y %H:%M',
-:standard_time => '%d %b %Y %H:%M'
-)
