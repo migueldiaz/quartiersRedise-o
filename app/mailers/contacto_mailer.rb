2982e1a05276131bb46715b5b67d81fedac55110
juanantonioruz <juanantonioruz@gmail.com>
2011-10-28 11:14:33 +0300

    Update app/mailers/contacto_mailer.rb

--- app/mailers/contacto_mailer.rb
+++ app/mailers/contacto_mailer.rb
@@ -1,5 +1,5 @@
 class ContactoMailer < ActionMailer::Base
-   default from: "donotreply@quartiersdumonde.org"
+  # default from: "donotreply@quartiersdumonde.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
