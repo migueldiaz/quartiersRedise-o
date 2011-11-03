90fc280056098fb04dfc6f54915372ebb1ae2951
Miguel <migueldiazgajete@gmail.com>
2011-11-03 11:49:22 +0100

    comprobacion email foros

--- app/models/usuarioforo.rb
+++ app/models/usuarioforo.rb
@@ -4,9 +4,32 @@ class Usuarioforo < ActiveRecord::Base
   belongs_to :equipo
   has_many :comentarios
  # validates_uniqueness_of :email
-  def tipo
-    ""
-  end
+  validate :validaemail,:on=>:create
+
+  def validaemail
+
+    if jeunes_id
+   	  @jeunes=Jeunes.first
+   	  @usuarios=@jeunes.usuarioforos
+
+   	elsif femmes_id
+   	  @femmes=Femmes.first
+   	  @usuarios=@femmes.usuarioforos
+    end
+
+    localizado=false
+
+    @usuarios.each do |usuario|
+       if usuario.email==email
+            localizado=true
+
+        end
+     end
+   	if  localizado==true
+   	  errors.add_to_base 'emailenuso'
+   	end
+  
+	end
   # el password no se utiliza, el usuario se confronta con el mail y la clave es de foro jeunes/femme
    def self.authenticate(email, password)
     usuario = find_by_email(email)
