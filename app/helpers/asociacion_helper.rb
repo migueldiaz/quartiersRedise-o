e864cc781dbfcf9ed38b112954c503569caa7846
juanantonioruz <juanantonioruz@gmail.com>
2012-03-16 16:56:28 +0100

    menus superiores resueltos y aplicando css a actualidades

--- app/helpers/asociacion_helper.rb
+++ app/helpers/asociacion_helper.rb
@@ -25,9 +25,9 @@ end
 def dame_class_central
   if content_for?(:menu_izq) and content_for?(:menu_dcha)
   "span-13 contenido_central"
-  elsif !content_for?(:menu_dcha)
+  elsif !content_for?(:menu_dcha) and content_for?(:menu_izq)
   "span-17 contenido_central last"
-  elsif !content_for?(:menu_izq)
+  elsif !content_for?(:menu_izq) and content_for?(:menu_dcha)
   "span-17 contenido_central "
   else
     "span-23 contenido_central last"
@@ -39,5 +39,16 @@ def dame_enfoque_paginas
   id_enfoque=Enfoque.where("sitio_id= :id_sitio",{:id_sitio=>id_sitio}).first.id
   Pagina.where("enfoque_id=:e_id", {:e_id=>id_enfoque})
 end
+def dame_quienes_somos_paginas
+  id_sitio=Sitio.where("asociacion_id=1").first.id
+  id_presentacion=Presentacion.where("sitio_id= :id_sitio",{:id_sitio=>id_sitio}).first.id
+  Pagina.where("presentacion_id=:e_id", {:e_id=>id_presentacion})
+end
+def dame_protagonistas_paginas
+  id_sitio=Sitio.where("asociacion_id=1").first.id
+  id_protagonistas=Protagonistas.where("sitio_id= :id_sitio",{:id_sitio=>id_sitio}).first.id
+  Pagina.where("protagonistas_id=:e_id", {:e_id=>id_protagonistas})
+end
+
 
 end
